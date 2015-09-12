140e98e8c19fe1eb93e34a053a1b7a17f7ffc94d
BhaaL <bhaal@0x1337.org>
2015-09-12 18:04:40 +0200

    actually calculate x/y directions from MAXIMUM VDC EXTENT
    this should also take VDC EXTENT into account, since the max. extent is
    actually optional

--- CgmInfoGui/Traversal/GraphicalElementBuilderVisitor.cs
+++ CgmInfoGui/Traversal/GraphicalElementBuilderVisitor.cs
@@ -12,7 +12,7 @@ namespace CgmInfoGui.Traversal
     {
         public override void AcceptMetafileDescriptorMaximumVdcExtent(MaximumVdcExtent maximumVdcExtent, GraphicalElementContext parameter)
         {
-            parameter.Visuals.VdcExtent = new Rect(maximumVdcExtent.FirstCorner.ToPoint(), maximumVdcExtent.SecondCorner.ToPoint());
+            parameter.SetExtent(maximumVdcExtent.FirstCorner.ToPoint(), maximumVdcExtent.SecondCorner.ToPoint());
         }
 
         public override void AcceptGraphicalPrimitivePolyline(Polyline polyline, GraphicalElementContext parameter)
